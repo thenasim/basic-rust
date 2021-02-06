80c0aa8a5255b6970969d35e44514789fba09a4b
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-02-06 13:40:00 +0600

    loops

---
+++ src/loops.rs
@@ -0,0 +1,33 @@
+pub fn run() {
+  // let mut count = 0;
+
+  // ? Infinite loop
+  // loop {
+  //   count += 1;
+  //   println!("Number: {}", count);
+  //   if count >= 10 {
+  //     break;
+  //   }
+  // }
+
+  // ? While Loop (FizzBuzz)
+  // while count <= 100 {
+  //   if count % 15 == 0 {
+  //     println!("fizzbuzz");
+  //   } else if count % 3 == 0 {
+  //     println!("fizz");
+  //   } else if count % 5 == 0 {
+  //     println!("buzz")
+  //   } else {
+  //     println!("{}", count);
+  //   }
+
+  //   // Inc
+  //   count += 1;
+  // }
+
+  // ? For range
+  for x in 0..20 {
+    println!("Range: {}", x);
+  }
+}
